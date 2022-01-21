f6624d03d5b496f4b93fa73ac892faa5bf54dacf
AntonBrightson <89799703+AntonBrightson@users.noreply.github.com>
2022-01-21 22:59:52 +0000

    Add files via upload

---
+++ anton-brightson/hw0_cpp/hw0_cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, I am all set up for C++ for ICP" << std::endl;
+}
---
+++ anton-brightson/hw0_cpp/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello, I am all set up for C++ for ICP\n";
+  return 0;
+} 
